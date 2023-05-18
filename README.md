a3b47125cda4df96894957c138f16bba3ecc5595
nozwock <57829219+nozwock@users.noreply.github.com>
2023-05-18 08:11:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PngWhisper
+
+An implementation of [this project idea](https://picklenerd.github.io/pngme_book/introduction.html).
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use anyhow::Result;
+
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod png;
+
+fn main() -> Result<()> {
+    todo!()
+}
